0aa45940a72500529a7ba9b063a5791ab6b4a3ee
Mar-Aze <97227237+Mar-Aze@users.noreply.github.com>
2022-06-08 16:13:22 +0200

    middleware auth & multer

--- back/routes/post.js
+++ back/routes/post.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
 const postCtrl = require('../controllers/post');
 
-router.post('/wall/posts', postCtrl.createPost);
+router.post('/wall/posts', auth, multer, postCtrl.createPost);
 
 
 module.exports = router;
