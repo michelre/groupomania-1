1d37507ea66b0be0f8c827611846cd6ce8548fbb
Mar-Aze <97227237+Mar-Aze@users.noreply.github.com>
2022-06-08 16:14:12 +0200

    multer route user

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
+const multer = require('../middleware/multer-config');
+
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', multer, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
