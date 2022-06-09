fd1f2ce100303cf93f7c791c1b08be172a0980c8
Mar-Aze <97227237+Mar-Aze@users.noreply.github.com>
2022-06-09 10:50:35 +0200

    routes et controllers users

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -3,24 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
-
-/*exports.signin = (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash,
-          firstName: req.body.firstName,
-          department: req.body.department,
-          imageUrl: req.body.imageUrl,
-        });
-        User.create(user) 
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };*/
-
+//Route Post/Création d'un utilisateur
   exports.signin = (req, res) => {
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
@@ -38,6 +21,7 @@ require('dotenv').config();
       .catch(error => res.status(500).json({ error }));
   };
 
+//Route Post/Connexion d'un utilisateur
   exports.login = (req, res) => {
     User.findOne({ where: {email: req.body.email }})
       .then(user => {
@@ -62,12 +46,53 @@ require('dotenv').config();
       .catch(error => res.status(500).json({ error }));
   };
 
+//Route Delete/Supprétion d'un utilisateur  
   exports.deleteUser = (req, res) => {
     const id = req.params.id;
-    User.destroy({where: {userId: id}})
-    .then(num => {
-      if (num === 1) {
-        res.status(204).end()
-      }
+    User.findOne({ where: {id: id }})
+      .then(user => {
+        if(user.id !== req.auth.id) {
+          res.status(403).json({message: "requête non autorisée!"});
+        } else {
+          const filename = user.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+            User.destroy({where: {id: id}})
+              .then(() => res.status(204).end())
+              .catch(error => res.status(400).json({ error }));
+          });
+        } 
+    })
+    .catch((error) => {
+      res.status(404).json({error: error})
     });
-  }
+  };
+
+//Route Put/Modification d'un utilisateur  
+exports.modifyUser = (req, res) => {
+  User.findOne({where: {id: req.params.id }})
+    .then(user => {  
+      if (user.id !== req.auth.id) {
+        res.status(403).json({ message: "requête non autorisée!"});
+      } 
+      if (!req.file) {
+        User.update({where :{ id: req.params.id }}, { ...req.body, id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Utilisateur modifié!'}))
+        .catch(error => res.status(400).json({ error }))
+      }
+      else {
+        const filename = user.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+        const userObject = req.file ?
+        {
+          ...JSON.parse(req.body.user),
+          imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+          User.update({where:{ id: req.params.id }}, { ...userObject, id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Utilisateur modifié!'}))
+            .catch(error => res.status(400).json({ error }))
+      })};      
+    })
+    .catch((error) => {
+        res.status(404).json({error: error})
+    });
+  };
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const userCtrl = require('../controllers/user');
 
 router.post('/signin', multer, userCtrl.signin);
 router.post('/login', userCtrl.login);
+router.put('/users/:id', auth, multer, userCtrl.modifyUser);
+router.delete('/users/:id', auth, multer, userCtrl.deleteUser);
 
 module.exports = router;
